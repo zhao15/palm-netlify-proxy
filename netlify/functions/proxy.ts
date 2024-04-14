928637750ee8f2f361369749e03895f7430fca36
zhao15 <zhaoyaowu12345@icloud.com>
2024-04-14 19:11:54 +0800

    Update proxy.ts

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -68,7 +68,7 @@ export default async (request: Request, context: Context) => {
   const response = await fetch(url, {
     body: request.body,
     method: request.method,
-    duplex: 'full',
+    duplex: 'half',
     headers,
   });
 
